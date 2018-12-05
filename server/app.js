7408f9fed2c23bd55f95e4fced3a5305528dfb62
OlegPanasyuk <oleg.panasyuk2@inbox.ru>
2018-12-05 16:47:44 +0300

    Add mysql module. And made request 'users' witch send list of users in table 'user'.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const http = require('http');
+const mysql = require('mysql');
 
 const routers = require('../server/middleware/routers');
+
+let con = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'mysql',
+  password: 'mysql',
+  database: 'mydb'
+});
+
+
 
 let app = express();
 app.use(express.static('../client'));
 
+app.get('/users', function (req, res, next) {
+  //res.send('get users');
+  con.connect((err) => {
+    if (err) throw err;
+    console.log('Connected');
+  });
+  
+  con.query("SELECT * FROM user", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    res.send(result);
+  });
+  con.end((err) => {
+    if (err) throw err;
+    console.log('end connettion');
+  });
+});
+
+app.post('/users', function (req, res, next) {
+  res.send('post users');
+});
 
 
 
